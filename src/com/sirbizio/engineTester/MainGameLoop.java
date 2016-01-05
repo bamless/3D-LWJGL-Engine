45d8ca0a59dfa9fc49425244c4b33ecd33dcda67
fabrizio <fabrizio@Fabrizio-Laptop>
2016-01-05 21:51:15 +0100

    Implemented obj file parser to load models from file
    Signed-off-by: fabrizio <fabrizio@Fabrizio-Laptop>

--- src/com/sirbizio/engineTester/MainGameLoop.java
+++ src/com/sirbizio/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import com.sirbizio.models.RawModel;
 import com.sirbizio.models.TexuredModel;
 import com.sirbizio.renderEngine.DisplayManager;
 import com.sirbizio.renderEngine.Loader;
+import com.sirbizio.renderEngine.OBJLoader;
 import com.sirbizio.renderEngine.Renderer;
 import com.sirbizio.shaders.StaticShader;
 import com.sirbizio.textures.ModelTexture;
@@ -16,98 +17,20 @@ import com.sirbizio.textures.ModelTexture;
 public class MainGameLoop {
     public static void main(String[] args) {
 	DisplayManager.createDisplay();
-	
-	float[] vertices = {			
-			-0.5f,0.5f,-0.5f,	
-			-0.5f,-0.5f,-0.5f,	
-			0.5f,-0.5f,-0.5f,	
-			0.5f,0.5f,-0.5f,		
-			
-			-0.5f,0.5f,0.5f,	
-			-0.5f,-0.5f,0.5f,	
-			0.5f,-0.5f,0.5f,	
-			0.5f,0.5f,0.5f,
-			
-			0.5f,0.5f,-0.5f,	
-			0.5f,-0.5f,-0.5f,	
-			0.5f,-0.5f,0.5f,	
-			0.5f,0.5f,0.5f,
-			
-			-0.5f,0.5f,-0.5f,	
-			-0.5f,-0.5f,-0.5f,	
-			-0.5f,-0.5f,0.5f,	
-			-0.5f,0.5f,0.5f,
-			
-			-0.5f,0.5f,0.5f,
-			-0.5f,0.5f,-0.5f,
-			0.5f,0.5f,-0.5f,
-			0.5f,0.5f,0.5f,
-			
-			-0.5f,-0.5f,0.5f,
-			-0.5f,-0.5f,-0.5f,
-			0.5f,-0.5f,-0.5f,
-			0.5f,-0.5f,0.5f
-			
-	};
-	
-	float[] textureCoords = {	
-			0,0,
-			0,1,
-			1,1,
-			1,0,			
-			0,0,
-			0,1,
-			1,1,
-			1,0,			
-			0,0,
-			0,1,
-			1,1,
-			1,0,
-			0,0,
-			0,1,
-			1,1,
-			1,0,
-			0,0,
-			0,1,
-			1,1,
-			1,0,
-			0,0,
-			0,1,
-			1,1,
-			1,0
-
-			
-	};
-	
-	int[] indices = {
-			0,1,3,	
-			3,1,2,	
-			4,5,7,
-			7,5,6,
-			8,9,11,
-			11,9,10,
-			12,13,15,
-			15,13,14,	
-			16,17,19,
-			19,17,18,
-			20,21,23,
-			23,21,22
-	};
-	
 	Loader loader = new Loader();
 	StaticShader shader = new StaticShader();
 	Renderer renderer = new Renderer(shader);
 	
-	RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-	ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+	RawModel model = OBJLoader.loadObjModel("dragon", loader);
+	ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
 	TexuredModel texturedmodel = new TexuredModel(model, texture);
 	
-	Entity entity = new Entity(texturedmodel, new Vector3f(0f,0,-5f),0,0,0,2);
+	Entity entity = new Entity(texturedmodel, new Vector3f(100f,0,-50f),0,0,0,2);
 	
 	Camera camera = new Camera();
 	
 	while(!Display.isCloseRequested()) {
-	    entity.increaseRotation(03f, 1.5f, 1);
+	    entity.increaseRotation(0, 1, 0);
 	    camera.move();
 	    
 	    //render
--- src/com/sirbizio/entities/Camera.java
+++ src/com/sirbizio/entities/Camera.java
@@ -12,13 +12,17 @@ public class Camera {
 	
 	public void move() {
 		if(Keyboard.isKeyDown(Keyboard.KEY_W))
-			position.z -= 0.02f;
+			position.z -= 0.6f;
 		if(Keyboard.isKeyDown(Keyboard.KEY_D))
-			position.x += 0.02f;
+			position.x += 0.6f;
 		if(Keyboard.isKeyDown(Keyboard.KEY_A))
-			position.x -= 0.02f;
+			position.x -= 0.6f;
 		if(Keyboard.isKeyDown(Keyboard.KEY_S))
-			position.z += 0.02f;
+			position.z += 0.6f;
+		if(Keyboard.isKeyDown(Keyboard.KEY_UP))
+			position.y += 0.6f;
+		if(Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+			position.y -= 0.6f;
 	}
 	
 	public Vector3f getPosition() {
