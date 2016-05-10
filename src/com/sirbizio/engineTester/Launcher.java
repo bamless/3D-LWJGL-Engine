d8fb7e5e522ebef9f29cd6ffb652ff8bd61c7779
SirBizio <pietrucci.1657779@studenti.uniroma1.it>
2016-05-10 22:11:38 +0200

    .

--- src/com/sirbizio/engineTester/Launcher.java
+++ src/com/sirbizio/engineTester/Launcher.java
@@ -3,13 +3,14 @@ package com.sirbizio.engineTester;
 import com.sirbizio.application.LWJGLApplication;
 import com.sirbizio.application.LWJGLConfiguration;
 
+
 public class Launcher {
 	public static void main(String[] args) {
 		LWJGLConfiguration config = new LWJGLConfiguration();
 		config.limitFPS = 60;
-		config.width = 1366;
-		config.height = 768;
-		config.fullscreen = false;
+		config.width = 800;
+		config.height = 450;
+		config.fullscreen = true;
 		config.vsync = true;
 		LWJGLApplication.create(new Test(), config);
 	}
